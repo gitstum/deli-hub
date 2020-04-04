122ab06c3592213fcd602bec67a7bbcc667f8292
Ben <benpan2017@gmail.com>
2020-04-04 16:54:48 +0800

    import all features
    Figure a way to import all model at once in the "feature" package.

--- features/__init__.py
+++ features/__init__.py
@@ -1,1 +1,23 @@
-from . import * 
+import os
+import time
+
+'''
+def import_all_models(path='features'):
+
+    model_list = []
+
+    for x in os.listdir(path):
+        if x.endswith('.py'):
+            if x == '__init__.py' \
+            or x == 'all.py':
+                continue
+            model_list.append(x.replace('.py', ''))
+
+    for model in model_list:
+        command = 'from %s.%s import *' % (path, model)
+        exec(command)
+        print('%s | feature "%s" has been imported.' % (
+            (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())),
+            model)
+        )
+'''
---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+
+
+path = 'features'   # path may differ.
+model_list = []
+
+for x in os.listdir(path):
+	if x.endswith('.py'):
+		if x == '__init__.py': continue
+		model_list.append(x.replace('.py', ''))
+
+print('-' * 50)
+for model in model_list:
+	command = 'from %s import %s' % (path, model)
+	exec(command)
+	print(command)
+print('-' * 50)
+
+test_features.cal()
