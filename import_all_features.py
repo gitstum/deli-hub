3743fb79ece6dafd21799692081f7c13052fbb21
Ben <benpan2017@gmail.com>
2020-04-04 17:41:41 +0800

    import_all_features.py debug

--- import_all_features.py
+++ import_all_features.py
@@ -1,28 +1,30 @@
 import os
 
+"""Notice: Direct Use is Required
+
+These import codes won't work when not in top block.
+Neither in functions nor "if block" can import be successful.
+So you have to copy them at the top where needed.
+"""
+
+path = 'features'  # path may differ.
+model_list = []
+
+for x in os.listdir(path):
+	if x.endswith('.py'):
+		if x == '__init__.py': continue
+		model_list.append(x.replace('.py', ''))
+
+print('-' * 50)
+for model in model_list:
+	command = 'from %s import %s' % (path, model)
+	exec(command)
+	print(command)
+print('-' * 50)
+
 
 if __name__ == '__main__':
 
-	"""Notice: Direct Use is Required
+	# Test. And this is how to use:
+	test_features.cal()
 
-	When packed into a function, these import codes won't work outside the function.
-	So you have to copy them to where needed.
-	"""
-
-	path = 'features'   # path may differ.
-	model_list = []
-
-	for x in os.listdir(path):
-		if x.endswith('.py'):
-			if x == '__init__.py': continue
-			model_list.append(x.replace('.py', ''))
-
-	print('-' * 50)
-	for model in model_list:
-		command = 'from %s import %s' % (path, model)
-		exec(command)
-		print(command)
-	print('-' * 50)
-
-	# how to use - call the filename:
-	# test_features.cal()
--- test.py
+++ test.py
@@ -24,5 +24,8 @@ def test():
 
 
 if __name__ == '__main__':
+
+
+
     test_features.cal()
     test()
