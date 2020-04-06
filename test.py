d905085c6e3cc3d48d28d0729d15c8b85df15670
Ben <benpan2017@gmail.com>
2020-04-06 22:01:11 +0800

    multi-processing caculation upgraded
    Use agg_cal() in tools to recall back the original function. So it becomes easier for code maintenance, and this simplify every module in features.

--- test.py
+++ test.py
@@ -7,3 +7,14 @@ if __name__ == '__main__':
 
     test_features.cal()
 
+    print('------')
+
+    r1, r2, r3 = test_features.agg_cal(['test1', 121],
+                         ['test2', 212],
+                         ['test3', 343],
+                         process_num=2)
+    print('-' * 40)
+    print(r1)
+    print(r2)
+    print(r3)
+
